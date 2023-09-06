fe804d03145bcd45f211f5fe5b86eae1e0c833e4
Jayesh Muley <115483595+jayeshmuley10@users.noreply.github.com>
2023-09-06 11:48:00 +0530

    Count Symmetric Integers.cpp

--- Weekly Contest/361/Count Symmetric Integers.java
+++ Weekly Contest/361/Count Symmetric Integers.java
@@ -1,29 +1,20 @@
 class Solution {
-    
     public int getans(String s){
-        
         int sum=0;
         for(int i=0;i<s.length();i++){
-            
             sum+=s.charAt(i)-48;
         }
         return sum;
     }
-    
-    public int countSymmetricIntegers(int low, int high) {
-        
+    public int countSymmetricIntegers(int low, int high) {   
         int ans=0;
-        
         while(low<=high){
-            
             String s=low+"";
-            
             if(s.length()%2==0){
                 int len=s.length();
                 int half=len/2;
                 String left=s.substring(0,half);
                 String right=s.substring(half,len);
-                
                 int sum=getans(left);
                 int sum2=getans(right);
                 if(sum==sum2)
@@ -31,7 +22,6 @@ class Solution {
             }
             low++;
         }
-        
         return ans;
     }
 }
